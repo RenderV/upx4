6d7de2639adcbd049272485dfc9b545283f497c1
RenderV <rmvmeira@gmail.com>
2023-10-16 14:00:53 -0300

    Revert "implement basic api functionality"
    This reverts commit cd60fb5dc9b503154e80e95501297cfc11cd599e.

--- backend/cams/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-
-from django.urls import path
-from .views import CameraViewSet, ParkingSpaceViewSet
-from rest_framework.routers import DefaultRouter
-router = DefaultRouter()
-router.register(r'cams', CameraViewSet, basename="cam")
-router.register(r'parking_spaces', ParkingSpaceViewSet, basename="parking_space")
-urlpatterns = router.urls
