e109a8e9d139f279e886682a5a98827d50029731
RenderV <rmvmeira@gmail.com>
2023-11-21 15:54:31 -0300

    remove unnecessary comments and add migrations

--- api/cams/admin.py
+++ api/cams/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Camera#, ParkingSpace, Point
-
-# Register your models here.
+from .models import Camera, ParkingSpace, Runtime, Record, ObjectTypes
 
 admin.site.register(Camera)
-# admin.site.register(ParkingSpace)
-# admin.site.register(Point)
+admin.site.register(ParkingSpace)
+admin.site.register(Runtime)
+admin.site.register(Record)
+admin.site.register(ObjectTypes)
